a1b879bb3de26b5f5cf4718a6bf33043a1f784cd
Praveen Reghunathan Nair <6327769+praveen-reghunath@users.noreply.github.com>
2021-03-30 10:30:51 -0700

    Intial setup for the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+
+function App() {
+  return (
+    <main className="App">
+      <header>
+        The Header
+      </header>
+      <footer>
+        Footer
+      </footer>
+    </main>
+  );
+}
+
+export default App;
